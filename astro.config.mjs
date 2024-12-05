c05575fd6c94b8765845e450a83811fcc297ff35
Unnunoctio <unnunoctio.dev@gmail.com>
2024-12-05 20:49:02 -0300

    refactor: :tada: restart project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
-import db from "@astrojs/db";
-import vercel from "@astrojs/vercel/serverless";
-
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), db(), alpinejs()],
-  output: "server",
-  adapter: vercel(),
-  experimental: {
-    serverIslands: true
-  }
-});
+export default defineConfig({});
--- db/seed.ts
+++
@@ -1,5 +0,0 @@
-
-// https://astro.build/db/seed
-export default async function seed() {
-	// TODO
-}
