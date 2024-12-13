c12d346d9d0153776f8c62691f23ecb08b036c50
Unnunoctio <unnunoctio.dev@gmail.com>
2024-12-13 16:44:02 -0300

    feat: :wrench: update config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  output: "server",
+  adapter: vercel()
+});
